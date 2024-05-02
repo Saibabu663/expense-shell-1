07fb623b81247652736d430d52f38256d1e99885
saibabu663 <saibabu663@gmail.com>
2024-05-02 14:04:06 +0530

    functions

--- backend.sh
+++ backend.sh
@@ -1,33 +1,11 @@
 #!/bin/bash
 
-USERID=$(id -u)
-TIMESTAMP=$(date +%F-%H-%M-%S)
-SCRIPT_NAME=$(echo $0 | cut -d "." )                                                                                                            -f1
-LOGFILE=/tmp/$SCRIPT_NAME-$TIMESTAMP.log
-R="\e[31m"
-G="\e[32m"
-y="\e[33m"
-N="\e[0m"
+
 echo "Please enter DB password:"
-read -s mysql_root-password
+read -s mysql_root_password
 
-VALIDATE(){
-   if [ $1 -ne 0 ]
-   then
-        echo -e "$2...$R FAILURE $N"
-        exit 1
-    else
-        echo -e "$2...$G SUCCESS $N"
-    fi
-}
-
-if [ $USERID -ne 0 ] 
-then     
-    echo "please run this script with root access."
-    exit 1 # manually exit if error comes.
-    else
-        echo "you are super user."
-fi
+echo "Please enter DB password:"
+read -s mysql_root_password
 
   dnf module disable nodejs -y &>>$LOGFILE
   VALIDATE $? "Disabling default nodejs"
--- mysql.sh
+++ mysql.sh
@@ -1,27 +1,27 @@
 #!/bin/bash
 
-source./common.sh
+source ./common.sh
 
 check_root
 
-echo "Please enter DB password:"
+echo "please enter DB password:"
 read -s mysql_root_password
   
-  dnf install mysql-server -y &>>$LOGFILE
-  VALIDATE $? "Installing MYSQL Server"
+  dnf install mysql-serddver -y &>>$LOGFILE
+  #VALIDATE $? "Installing MYSQL Server"
 
   systemctl enable mysqld &>>$LOGFILE
-  VALIDATE $? "Enabling MYSQL Server"
+  #VALIDATE $? "Enabling MYSQL Server"
 
  systemctl start mysqld  &>>$LOGFILE
-  VALIDATE $? "Starting MYSQL Server"
+ # VALIDATE $? "Starting MYSQL Server"
 
 #  mysql_secure_installation --set-root-pass ExpenseApp@1 &>>$logfile
-#  VALIDATE $? "etting up root password"
+#  VALIDATE $? "setting up root password"
 
 
 #Below code will be useful for idempotent nature
- mysql -h db.daws-78s.online -uroot -pExpenseApp@1 -e 'SHOW  DATABASES
+ mysql -h db.daws78s.online -uroot -pExpenseApp@1 -e 'SHOW  DATABASES
  if [ $? -ne 0 ]
  then
     mysql_secure_installation --set-root-pass ExpenseApp@1 &>>$LOGFILE
