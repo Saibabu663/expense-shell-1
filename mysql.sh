9aee6d7e3f14682a2e9ecc89addbc64e7b7609af
saibabu663 <saibabu663@gmail.com>
2024-05-02 14:16:17 +0530

    functions

--- mysql.sh
+++ mysql.sh
@@ -8,17 +8,13 @@ echo "please enter DB password:"
 read -s mysql_root_password
   
   dnf install mysql-serddver -y &>>$LOGFILE
-  #VALIDATE $? "Installing MYSQL Server"
+  
 
   systemctl enable mysqld &>>$LOGFILE
-  #VALIDATE $? "Enabling MYSQL Server"
+ 
 
  systemctl start mysqld  &>>$LOGFILE
- # VALIDATE $? "Starting MYSQL Server"
-
-#  mysql_secure_installation --set-root-pass ExpenseApp@1 &>>$logfile
-#  VALIDATE $? "setting up root password"
-
+ 
 
 #Below code will be useful for idempotent nature
  mysql -h db.daws78s.online -uroot -pExpenseApp@1 -e 'SHOW  DATABASES
